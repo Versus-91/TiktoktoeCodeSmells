7eff2d4591bff70da7c457633f3da941ea021a65
Ghader <gharibyan.ghader@gmail.com>
2025-06-09 11:21:41 +0200

    separating classes

---
+++ TicTacToe/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Tile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Symbol { get; set; }
+    }
+}
